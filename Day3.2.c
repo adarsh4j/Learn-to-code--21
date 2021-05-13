59a7fe998e727e4f2bb6ec02c63c86cf8925eda2
Adarshq2-shi <78248655+Adarshq2-shi@users.noreply.github.com>
2021-05-13 15:54:00 +0530

    Create Day3.2.c

---
+++ Day3.2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+      float f;
+      scanf("%f",&f);
+      
+      printf("%.2f \n",(f-32)*5/9);
+
+}
