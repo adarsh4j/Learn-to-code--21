72501fabd9a5d25b2217cebec5372038438db7b6
Adarshq2-shi <78248655+Adarshq2-shi@users.noreply.github.com>
2021-05-13 14:21:24 +0530

    Create Day3.1.c

---
+++ Day3.1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+    int a;
+    float b,c,d;
+    scanf("%d",&a);
+    scanf("%f",&b);
+    scanf("%f",&c);
+    scanf("%f",&d);
+    
+    printf("%d \n",a*a);
+    printf("%.1f \n",3.14*b*b);
+    printf("%.2f \n",0.5*c*d);
+    
+
+}
