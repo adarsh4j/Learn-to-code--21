6fcb9a12301732fe3ada8740236d724946af7bdb
Adarshq2-shi <78248655+Adarshq2-shi@users.noreply.github.com>
2021-05-14 14:50:00 +0530

    Create Day4.1.c

---
+++ Day4.1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+    int a;
+    scanf("%d",&a);
+    
+    if(a%400==0)
+    
+
+          
+    printf("Yes");
+           
+    else 
+    
+    if(a%4==0&&a%100!=0)
+          
+    printf("Yes");
+    else    
+
+     printf("No");
+
+}
+    
