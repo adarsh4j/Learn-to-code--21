0b68a6b2b0197bdb42a126f7a51cdf061004d380
Adarshq2-shi <78248655+Adarshq2-shi@users.noreply.github.com>
2021-05-19 20:38:50 +0530

    Create Day7.1.c

---
+++ Day7.1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    switch(a)
+    {
+      case 1 : printf("Sunday");
+             break;
+      case 2 : printf("Monday");
+             break;
+      case 3 : printf("Tuesday");
+             break;
+      case 4 : printf("Wednesday");
+             break;
+      case 5 : printf("Thursday");
+             break;
+      case 6 : printf("Friday");
+             break;
+      case 7 : printf("Saturday");
+             break;
+      default : printf("Error");
+             break;
+     }
+     
+    return 0;
+}
