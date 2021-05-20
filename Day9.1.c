462534f1d489bf1963edba505285561ed01449dd
Adarshq2-shi <78248655+Adarshq2-shi@users.noreply.github.com>
2021-05-20 16:09:22 +0530

    Create Day9.1.c

---
+++ Day9.1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{ 
+   int a;
+   int b;
+   
+   
+    scanf("%d",&a);
+    scanf("%d",&b);
+    
+    printf("2");
+    
+
+     
+    return 0;
+}
